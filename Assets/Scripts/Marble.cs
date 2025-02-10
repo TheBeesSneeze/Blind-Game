0437882801a3aada0853c328890ee5bfc33e6430
Jay Embry <kembry@mail.bradley.edu>
2025-02-10 11:48:11 -0600

    yippee!!!!!!!!!!

--- Assets/Scripts/Marble.cs
+++ Assets/Scripts/Marble.cs
@@ -4,15 +4,84 @@ using UnityEngine;
 
 public class Marble : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Rigidbody rb;
+
+    [Header("Stats")]
+
+    [Tooltip("How loud is the object?")]
+    public float sound;
+
+    [Tooltip("Exact string from sfx manager")]
+    public string nameOfSfx;
+
+    [Tooltip("At what point will the marble disappear from the scene?")]
+    public float minimumVelocity;
+
+    [Tooltip("Add whatever layers that this object should collide with here!")]
+    public LayerMask surfaces;
+
+    [Tooltip("Edit this object's soundwave here!")]
+    public SoundWaveProperties waves;
+
+    [Tooltip("Scales the soundwave in relation to the marble's velocity.")]
+    public float WaveScalar;
+
+    //thank you marissa
+
+    [Header("Throwing")]
+
+    [SerializeField] int throwForce;
+    [SerializeField] TrailRenderer trailRenderer;
+    public Material TrailMaterial;
+
     void Start()
     {
-        
+
+        rb = GetComponent<Rigidbody>();
+
+        trailRenderer = GetComponent<TrailRenderer>();
+        if (trailRenderer == null)
+        {
+            trailRenderer = gameObject.AddComponent<TrailRenderer>();
+            trailRenderer.endWidth = 0;
+            trailRenderer.material = TrailMaterial;
+        }
+        trailRenderer.enabled = false;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+
+        if(rb.velocity.magnitude <= minimumVelocity)
+        {
+
+            Destroy(this.gameObject);
+
+        }
+
+    }
+
+    public void ThrowMarble(Vector3 direction)
+    {
+
+        rb.AddForce(direction * 100 * throwForce);
+        trailRenderer.enabled = true;
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+
+        if (surfaces == (surfaces | (1 << collision.gameObject.layer)))
+        {
+
+            SfxManager.Instance.PlaySFX(nameOfSfx);
+            waves.MaxRadius = rb.velocity.magnitude * WaveScalar;
+            waves.PlayAtPosition(collision.contacts[0].point);
+
+        }
+
     }
 }
